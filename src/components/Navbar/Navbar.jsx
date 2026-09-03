881e02aded771a7ff6336cc271c2de144fc2a566
CorillaFrank <jhoncorilla2014.com>
2026-09-03 12:05:38 -0500

    chore:new changes for the log out

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,77 @@
 import "./Navbar.css";
 import logoGM from "../../assets/logo-mg.png";
-
+import { useState } from "react";
+import ProfileMenu from "../ProfileMenu/ProfileMenu";
 function Navbar({ usuario }) {
+
+
+  const [profileOpen, setProfileOpen] = useState(false);
+
+
+
   return (
+
     <header className="navbar-principal">
 
+
       <div className="navbar-logo">
+
         <img
           src={logoGM}
           alt="GM Ingenieros y Consultores"
         />
+
       </div>
+
+
 
       <div className="navbar-info">
-        <span>{usuario?.nombre}</span>
 
-        <div className="navbar-avatar">
-          {usuario?.nombre?.charAt(0)}
+
+        <span>
+          {usuario?.nombre}
+        </span>
+
+
+
+        <div className="profile-container">
+
+
+          <button
+            className="navbar-avatar"
+            onClick={() => setProfileOpen(!profileOpen)}
+          >
+
+            {usuario?.nombre?.charAt(0)}
+            {usuario?.apellido?.charAt(0)}
+
+          </button>
+
+
+
+          {
+            profileOpen && (
+
+              <ProfileMenu
+                usuario={usuario}
+              />
+
+            )
+          }
+
+
+
         </div>
+
+
       </div>
 
+
     </header>
+
   );
+
 }
+
 
 export default Navbar;
